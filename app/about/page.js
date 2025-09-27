620418e89e0ea4af0f4b92b557f6146241113057
John Salerno <john3789@gmail.com>
2025-09-27 19:47:04 -0400

    mk

--- app/about/page.js
+++ app/about/page.js
@@ -14,7 +14,7 @@ export default function AboutPage() {
 <p className="space-y-6 text-lg mb-4 opacity-90 leading loose">
 
  Dr. Juan Pablo Salerno, also known in academic spaces as <strong>Dr. John P. Salerno</strong>, is a mental health scientist, 
- personal growth expert, and 30x scientific peer-reviewed author, cited 
+ personal growth expert, and 30x published scientific peer-reviewed author, cited 
  nearly 2000x in the last decade (2016 - present). He has over 14 years of experience working in top academic 
  institutions, including Columbia University, George Washington University, 
  University of Maryland, and University of Miami. He is committed to sharing 
