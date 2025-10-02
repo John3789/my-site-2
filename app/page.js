b51151960fa2e630fe8aa33b57f46636c662a815
John Salerno <john3789@gmail.com>
2025-10-02 16:38:00 -0400

    g

--- app/page.js
+++ app/page.js
@@ -19,21 +19,24 @@ export default function Home() {
           </h1>
         </div>
       </section>
-     <div
-  style={{ '--z': 3.00 }} // tweak 1.08–1.20 depending on how close you want it
-  className="
-    md:contents
-    origin-top
-    [transform:scale(var(--z))]
-    [width:calc(100%/var(--z))]
-    mx-auto
-    md:[transform:none]
-    md:[width:100%]
-    overflow-x-hidden
-        /* compensation so footer stays below on mobile */
-    mb-[calc((var(--z)-1)*200dvh)] md:mb-0
-  "
->
+
+
+      {/* Hero zoom wrapper */}
+      <div
+        style={{ '--hz': 3.00 }}
+        className="
+          md:contents
+          origin-top
+          [transform:scale(var(--hz))]
+          [width:calc(100%/var(--hz))]
+          mx-auto
+          md:[transform:none]
+          md:[width:100%]
+          overflow-x-hidden
+        "
+      >
+        </div>
+
       {/* Hero with background photo */}
       <section className="relative h-[88.8svh] overflow-hidden">
         <Image
@@ -303,7 +306,6 @@ export default function Home() {
   </div>
 </section>
       </main>
-      </div>
       </div>
     </>
   )
