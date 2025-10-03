a116eea4286464a6b3a3937f5ed681ab8e432342
John Salerno <john3789@gmail.com>
2025-10-02 23:55:47 -0400

    k

--- app/page.js
+++ app/page.js
@@ -34,7 +34,7 @@ export default function Home() {
             sizes="(min-width:768px) 100vw, 150vw"
             className="
               object-cover origin-center will-change-transform
-              [transform:scaleX(1.04)_scaleY(1.02)]   /* mobile subtle zoom */
+              [transform:scaleX(1.06)_scaleY(1.00)]   /* mobile subtle zoom */
               md:[transform:none]                     /* desktop unchanged */
               object-[center_6%] md:object-[center_0%]
             "
