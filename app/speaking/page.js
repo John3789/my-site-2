e30a19f15977dd31a564055e8c82b4a4b4208acd
John Salerno <john3789@gmail.com>
2025-10-04 18:52:55 -0400

    g

--- app/speaking/page.js
+++ app/speaking/page.js
@@ -231,7 +231,7 @@ const jump = (id, opts = {}) => {
           </section>
 
           {/* ===== MAIN CONTENT ===== */}
-          <div className="mx-auto max-w-[1400px] py-6 space-y-16 md:space-y-24" id="topics">
+          <div className="mx-auto max-w-[1400px] py-6 space-y-12 md:space-y-24" id="topics">
             <hr className="border-t border-[var(--color-cream)]/22 md:mb-8 w-full" />
 
             {/* Popular Topics (Programs) */}
