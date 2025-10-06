b9c4da3bc45f759a06ea05d5ce5ee9986a00209f
John Salerno <john3789@gmail.com>
2025-10-06 18:38:11 -0400

    t

--- app/meditations/page.js
+++ app/meditations/page.js
@@ -155,7 +155,7 @@ export default function MeditationsPage() {
           <div className="pb-10" />
 
           {/* --- MOBILE divider + footer (inside zoom, after the form) --- */}
-<div className="mx-auto max-w-[1100px] px-6 mt-0">
+<div className="mx-auto max-w-[1100px] px-12 -mt-4">
   <hr className="border-t border-[var(--color-cream)]/22 mb-15" />
 </div>
 
