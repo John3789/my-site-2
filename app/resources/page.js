98b482cdce63ab7a13fefaed116c17f1a037eb0d
John Salerno <john3789@gmail.com>
2025-10-07 19:24:18 -0400

    h

--- app/resources/page.js
+++ app/resources/page.js
@@ -229,11 +229,11 @@ export default function ResourcesPage() {
               ))}
             </div>
 
-<div className="md:hidden mx-auto max-w-[2000px] -px-3 mt-13">
+<div className="md:hidden mx-auto max-w-[2000px] -px-6 mt-13">
   <hr className="border-t border-[var(--color-cream)]/22 mb-15" />
 </div>
 
-<div className="md:hidden mx-auto max-w-[2000px] -mb-20 -px-3">
+<div className="md:hidden mx-auto max-w-[2000px] -mb-20 -px-6">
   {/* Newsletter card (midnight blue) */}
   <div className="rounded-xl bg-[#0f2334] ring-1 ring-white/10 p-5 shadow-2xl mt-10">
     <p className="text-[12px] uppercase tracking-[0.18em] opacity-70 mb-2">
