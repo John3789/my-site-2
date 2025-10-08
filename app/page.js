ec6592785c0fe615bfc19e4970ba5e6641aa3d3b
John Salerno <john3789@gmail.com>
2025-10-08 18:32:16 -0400

    s

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import Image from "next/image";
 import NewsletterMeditationPopup from "../components/NewsletterMeditationPopup";
-import SocialFooter from "../components/SocialFooter";
 import NewsletterSignup from "../components/NewsletterSignup";
 
 
