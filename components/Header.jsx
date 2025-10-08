ea7a8b31909aca8d86da8fdbb4eda8faf8472aa5
John Salerno <john3789@gmail.com>
2025-10-07 22:14:28 -0400

    c

--- components/Header.jsx
+++ components/Header.jsx
@@ -83,7 +83,7 @@ export default function Header() {
     "
   >
     <span className="hidden text-lg lg:text-xl">DR. JUAN PABLO SALERNO</span>
-    <sup className="text-lg align-super opacity-70">™</sup>
+    <sup className="hidden text-lg align-super opacity-70">™</sup>
   </Link>
 )}
 
@@ -102,7 +102,7 @@ export default function Header() {
     <aside
       className="
         fixed inset-0 z-[10001]
-        !bg-[var(--color-teal-800)] text-[var(--color-cream)]
+        !bg-[var(--color-teal-850)] text-[var(--color-cream)]
         flex flex-col
         min-h-[100svh]   /* fills viewport; stable on iOS */
         overflow-y-auto   /* scroll if long */
