f5124af78eb8adf163a9c466789a048301d57f68
John Salerno <john3789@gmail.com>
2025-10-09 17:28:10 -0400

    x

--- app/speaking/page.js
+++ app/speaking/page.js
@@ -876,7 +876,7 @@ export default function SpeakingPage() {
                 </div>
 
 {/* Toggle button */}
-<div className="mt-4 flex justify-center w-full">
+<div className="mt-4 flex justify-center w-full landscape:-mb-3">
   <button
     onClick={() => setShowAllTestimonials((s) => !s)}
     aria-expanded={showAllTestimonials}
