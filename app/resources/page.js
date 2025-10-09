395cff3cd9f0ee871c636ad79e480ca35949b30c
John Salerno <john3789@gmail.com>
2025-10-08 22:13:38 -0400

    d

--- app/resources/page.js
+++ app/resources/page.js
@@ -233,8 +233,10 @@ export default function ResourcesPage() {
 
 {/* --- DESKTOP divider above newsletter --- */}
 <div className="hidden lg:block mx-auto max-w-[1200px] px-6">
-  <hr className="border-t border-[var(--color-cream)]/22 mt-0 mb-0" />
+  <hr className="border-t border-[var(--color-cream)]/22 mt-0 mb-10" />
 </div>
+
+
 {/* --- MOBILE divider + footer (inside zoom, after the form) --- */}
 <div className="block lg:hidden">
   <div className="mx-auto max-w-[1400px] px-6 mb-14 -mt-5">
