6b7b2c8678ea23ef4062f2f90b28cbed1006a39c
John Salerno <john3789@gmail.com>
2025-10-08 22:33:07 -0400

    d

--- app/speaking/page.js
+++ app/speaking/page.js
@@ -944,7 +944,7 @@ export default function SpeakingPage() {
 
 {/* --- MOBILE divider + footer (inside zoom, after the form) --- */}
 <div className="block lg:hidden">
-  <div className="mx-auto max-w-[1400px] px-6 mb-14 -mt-5">
+  <div className="mx-auto max-w-[1400px] px-6 mb-0 mt-0">
   </div>
 
   <div className="mobile-footer-cap">
