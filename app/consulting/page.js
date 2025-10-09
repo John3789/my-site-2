075fa3ce117f58ec91de93e62d6a0dd72c758474
John Salerno <john3789@gmail.com>
2025-10-09 01:54:10 -0400

    g

--- app/consulting/page.js
+++ app/consulting/page.js
@@ -457,6 +457,8 @@ export default function ConsultingPage() {
             {/* ... */}
           </div>
 
+                  {/* removed the contained divider on mobile */}
+
           {/* --- MOBILE divider + footer (inside zoom, after the form) --- */}
           <div className="block lg:hidden">
             <div className="mx-auto max-w-[1400px] px-6 mb-14 -mt-5">
