a61a68c7270f97a09de92c4f11e146daeed8b4b8
John Salerno <john3789@gmail.com>
2025-10-08 22:18:56 -0400

    s

--- app/resources/page.js
+++ app/resources/page.js
@@ -239,7 +239,7 @@ export default function ResourcesPage() {
 
 {/* --- MOBILE divider + footer (inside zoom, after the form) --- */}
 <div className="block lg:hidden">
-  <div className="mx-auto max-w-[1400px] px-6 mb-14 mt-0">
+  <div className="mx-auto max-w-[1400px] px-6 mb-0 -mt-5">
     <hr className="border-t border-[var(--color-cream)]/22 mb-0" />
   </div>
 
