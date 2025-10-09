09b79feeb2d06175fb78c3948ddee5f8b8c0537c
John Salerno <john3789@gmail.com>
2025-10-08 20:30:47 -0400

    c

--- app/page.js
+++ app/page.js
@@ -301,13 +301,10 @@ export default function Home() {
                 </div>
               </div>
             </div>
-              <div className="mx-auto max-w-[1400px] px-6 mb-14 -mt-5">
-  </div>
-              <hr className="border-t border-[var(--color-cream)]/22 mb-6" />
-
 {/* --- MOBILE divider + footer (inside zoom, after the form) --- */}
 <div className="block lg:hidden mobile-footer-cap">
   <div className="mx-auto max-w-[1400px] px-6 mb-14 -mt-5">
+    <hr className="border-t border-[var(--color-cream)]/22 mb-0" />
   </div>
 
   <div className="mx-auto max-w-[1400px] px-3">
