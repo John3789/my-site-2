e43b74b1fa1b4bbfba95fde05af19e32428ec1b3
John Salerno <john3789@gmail.com>
2025-10-09 18:17:12 -0400

    jk

--- app/speaking/page.js
+++ app/speaking/page.js
@@ -273,7 +273,7 @@ export default function SpeakingPage() {
 
             {/* Popular Topics (Programs) */}
             <section
-               className="grid grid-cols-1 lg:grid-cols-12 gap-12 items-center mx-15 gap-y-12 portrait:mx-0 portrait:px-3 landscape:max-w-[760px] landscape:mx-auto landscape:px-3 lg:landscape:max-w-[1400px] lg:landscape:mx-15 lg:landscape:px-0"
+               className="grid grid-cols-1 lg:grid-cols-12 gap-12 items-center mx-15 gap-y-12 portrait:mx-0 portrait:px-6 landscape:max-w-[760px] landscape:mx-auto landscape:px-3 lg:landscape:max-w-[1400px] lg:landscape:mx-15 lg:landscape:px-0"
               
             >
               <div className="md:col-span-5 md:py-6">
