d639f8e8520a9f4094536f6d0ac5617e663b199a
John Salerno <john3789@gmail.com>
2025-10-09 12:14:42 -0400

    f

--- app/page.js
+++ app/page.js
@@ -110,7 +110,7 @@ export default function Home() {
 
       {/* ===== PAGE BODY WRAPPER ===== */}
       <div
-        style={{ '--z': 3.00, '--zoomL': 1.20 }}
+        style={{ '--z': 3.00, '--zoomL': 1.30 }}
         className="
           lg:contents
           origin-top
