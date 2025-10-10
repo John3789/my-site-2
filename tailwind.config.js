99267f1ebc8816807e68cbf7344526bfacd44ad0
John Salerno <john3789@gmail.com>
2025-10-10 17:02:05 -0400

    d

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        // Custom screen for tablet landscape
+        'tab-land': { 'raw': '(orientation: landscape) and (min-width: 1000px) and (max-width: 1400px)' },
+      },
+    },
+  },
+  plugins: [],
+};
