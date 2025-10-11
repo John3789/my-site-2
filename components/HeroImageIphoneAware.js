cc64b1a6f3c52799c1cb49bbf62cc4ef79543002
John Salerno <john3789@gmail.com>
2025-10-11 13:50:08 -0400

    l

--- components/HeroImageIphoneAware.js
+++ components/HeroImageIphoneAware.js
@@ -1,18 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
-/** Optimizes on iPhone only; everywhere else uses the original (unoptimized). */
+/** All images optimized (Next.js handles DPR & srcset automatically). */
 export default function HeroImageIphoneAware(props) {
-  const [isIphone, setIsIphone] = useState(false);
-
-  useEffect(() => {
-    const ua = navigator.userAgent || navigator.vendor || "";
-    setIsIphone(/\biPhone\b|\biPod\b/.test(ua));
-  }, []);
-
-  // On iPhone => optimized (unoptimized=false)
-  // Elsewhere => unoptimized (original file served)
-  return <Image {...props} unoptimized={!isIphone} />;
+  return <Image {...props} unoptimized={false} />;
 }
