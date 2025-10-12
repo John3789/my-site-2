44c42d8f3bcc19f02fd0ea0fa62666711d5fed24
John Salerno <john3789@gmail.com>
2025-10-12 16:11:49 -0400

    h

--- app/page.js
+++ app/page.js
@@ -77,7 +77,7 @@ export default function Home() {
     drop-shadow-[0_2px_6px_rgba(0,0,0,0.35)]
   "
   style={{
-    fontSize: 'clamp(28px,7.2vw,40px)', // ← adjust here and it WILL take effect
+    fontSize: 'clamp(32px,20vw,55px)', // ← adjust here and it WILL take effect
     lineHeight: 1,
     WebkitTextSizeAdjust: '100%',
   }}
