e4c5a4a6c15dd4d7655404355eff8b5edd68aa2a
John Salerno <john3789@gmail.com>
2025-10-13 12:32:43 -0400

    b

--- components/Header.jsx
+++ components/Header.jsx
@@ -54,7 +54,7 @@ export default function Header() {
   onClick={() => setOpen(true)}
   className="
   lg:hidden 
-  mr-auto -mr-1 -ml-0 p-0 translate-y-14
+  mr-auto -mr-1 -ml-0 p-0 translate-y-7
   landscape: -mr-1 ml-5 p-3           /* less left margin in landscape */
   landscape:translate-y-4  /* pull it up a bit in landscape */
   "
