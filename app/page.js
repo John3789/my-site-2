8bda77096e85c3c2679d88bc5229515131047c3e
John Salerno <john3789@gmail.com>
2025-10-13 14:35:58 -0400

    i

--- app/page.js
+++ app/page.js
@@ -345,7 +345,7 @@ export default function Home() {
           </div>
 
 {/* --- MOBILE divider + footer (inside zoom, after the form) --- */}
-<div className="block lg:hidden">
+<div className="block lg:hidden narrow-landscape-70">
   <div className="mx-auto max-w-[1400px] px-6 mb-14 -mt-5">
     <hr className="border-t border-[var(--color-cream)]/22 mb-0" />
   </div>
