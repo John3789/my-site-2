6525f40f7c32be6b217496ba135fe0f9065bbd98
John Salerno <john3789@gmail.com>
2025-10-16 15:22:33 -0400

    c

--- app/books/page.js
+++ app/books/page.js
@@ -374,7 +374,7 @@ export default function BooksPage() {
             
 {/* --- MOBILE divider + footer (inside zoom, after the form) --- */}
 <div className="block lg:hidden">
-  <div className="mx-auto max-w-[1400px] px-6 mb-14 -mt-5">
+  <div className="mx-auto max-w-[90%] px-6 mb-14 -mt-5">
     <hr className="border-t border-[var(--color-cream)]/22 mb-0" />
   </div>
 
