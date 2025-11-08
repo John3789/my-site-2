a00f2fd8f0419fe557178741505d0c667daf68b1
John Salerno <john3789@gmail.com>
2025-11-08 18:01:54 -0500

    y

--- app/Providers/MemberstackProvider.jsx
+++ app/Providers/MemberstackProvider.jsx
@@ -1,9 +1,7 @@
-// app/providers/MemberstackProvider.jsx
 "use client";
-import { MemberstackProvider } from "@memberstack/react"; 
+import { MemberstackProvider } from "@memberstack/react";
 
 export default function MSProvider({ children }) {
   const publicKey = process.env.NEXT_PUBLIC_MEMBERSTACK_PUBLIC_KEY || "pk_placeholder";
   return <MemberstackProvider publicKey={publicKey}>{children}</MemberstackProvider>;
 }
- 
