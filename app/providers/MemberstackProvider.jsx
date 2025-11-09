b823a59b6056c56d62b26efc8da05d90abc6e8b7
John Salerno <john3789@gmail.com>
2025-11-08 22:54:57 -0500

    e

--- app/providers/MemberstackProvider.jsx
+++ app/providers/MemberstackProvider.jsx
@@ -1,17 +1,11 @@
 // app/providers/MemberstackProvider.jsx
 "use client";
 
-import { MemberstackProvider as MS, MemberstackPortals } from "@memberstack/react";
+import { MemberstackProvider as MS } from "@memberstack/react";
 
 export default function MemberstackProvider({ children }) {
   const publicKey =
     process.env.NEXT_PUBLIC_MEMBERSTACK_PUBLIC_KEY || "pk_placeholder";
 
-  return (
-    <MS config={{ publicKey }}>
-      {/* This renders the LOGIN / SIGNUP modals into the DOM */}
-      <MemberstackPortals />
-      {children}
-    </MS>
-  );
+  return <MS config={{ publicKey }}>{children}</MS>;
 }
