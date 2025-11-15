ab402f0ce976d8abdc6f672ed33b6e68e839c421
John Salerno <john3789@gmail.com>
2025-11-15 13:41:14 -0500

    x

--- app/membership/BuyButton.jsx
+++ app/membership/BuyButton.jsx
@@ -2,8 +2,8 @@
 "use client";
 
 const PRICE_IDS = {
-  monthly: "prc_9-99-fees-8z9903dk",
-  yearly: "prc_89-99-fees-i1kr04by",
+  monthly: "prc_9-99-fees-7guk0999",
+  yearly: "prc_89-99-fees-xvwf0tc5",
 };
 
 export default function BuyButton({ cadence = "monthly", className = "", children }) {
